e18e1026576f02051739239e5ff6675debe116e0
Olena-Nechyporenko <127235568+Olena-Nechyporenko@users.noreply.github.com>
2023-12-19 16:22:51 +0200

    commit1

---
+++ controllers/contacts.js
@@ -0,0 +1,89 @@
+const Joi = require("joi");
+const Contact = require("../models/contact.js");
+
+const { HttpError } = require("../helpers/index");
+const ctrlWrapper = require("../helpers/ctrlWrapper");
+
+const schema = Joi.object({
+  name: Joi.string().min(2).max(30).required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.boolean(),
+});
+
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
+
+const getAllContacts = async (req, res) => {
+  const result = await Contact.find();
+  res.json(result);
+};
+
+const getById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findById(contactId);
+  if (!result) {
+    throw HttpError(404, "Not Found");
+  }
+  res.json(result);
+};
+
+const addContact = async (req, res) => {
+  const { error } = schema.validate(req.body);
+  if (error) {
+    throw HttpError(400, "Missing required name field");
+  }
+  const result = await Contact.create(req.body);
+  res.status(201).json(result);
+};
+
+const deleteContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndDelete(contactId);
+  if (!result) {
+    throw HttpError(404, "Not Found");
+  }
+  res.json({
+    message: "Contact deleted",
+  });
+};
+
+const updateById = async (req, res) => {
+  const { error } = schema.validate(req.body);
+  if (error) {
+    throw HttpError(400, "Missing fields");
+  }
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw HttpError(404, "Not Found");
+  }
+  res.json(result);
+};
+
+const updateStatusContact = async (req, res) => {
+  const { error } = updateFavoriteSchema.validate(req.body);
+  if (error) {
+    throw HttpError(400, "Missing field favorite");
+  }
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw HttpError(404, "Not Found");
+  }
+  res.json(result);
+};
+
+module.exports = {
+  getAllContacts: ctrlWrapper(getAllContacts),
+  getById: ctrlWrapper(getById),
+  addContact: ctrlWrapper(addContact),
+  deleteContact: ctrlWrapper(deleteContact),
+  updateById: ctrlWrapper(updateById),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,19 @@
-const express = require('express')
+const express = require("express");
+const router = express.Router();
 
-const router = express.Router()
+const ctrl = require("../../controllers/contacts");
+const isValidId = require("../../middlewares/isValidId");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", ctrl.getAllContacts);
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", isValidId, ctrl.getById);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", ctrl.addContact);
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", isValidId, ctrl.deleteContact);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", isValidId, ctrl.updateById);
 
-module.exports = router
+router.patch("/:contactId/favorite", isValidId, ctrl.updateStatusContact);
+
+module.exports = router;
